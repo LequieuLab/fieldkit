4f7be6583a4d2ea6be2505ee6aa8127eb54ced36
Joshua Lequieu <lequieu@drexel.edu>
2022-03-31 17:11:07 -0400

    fixing dependencies

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,8 @@
 import os
 import sys
 #sys.path.append('~/fieldkit/fieldkit')
+sys.path.append(os.path.abspath("."))
+sys.path.append(os.path.abspath("../"))
 sys.path.append(os.path.abspath("../../"))
 
 
