28dcef0126c7e455a521a089eeed182b6c961b4f
Joshua Lequieu <lequieu@drexel.edu>
2022-04-13 13:57:01 -0400

    new command line tool for plotting

---
+++ tools/fk_plot.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from fk_setpath import *
+import fieldkit as fk 
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Plot a field using matplotlib')
+parser.add_argument('infile',help='Input filename of field')
+parser.add_argument('-o','--outfile',default=None, help='Output filename of plot')
+parser.add_argument('--show',default=True,action='store_true', help='show plot via interactive window?')
+parser.add_argument('--dpi',default=100,help='resolution (dpi) of plot')
+args = parser.parse_args()
+#print(args)
+
+# read file
+fields = fk.read_from_file(args.infile)
+
+# plot
+fk.plot(fields, dpi=args.dpi, filename=args.outfile, show=args.show)
+
